91fbcfcefada33f02e069b72ffe945e02aa5c9e8
hendraes <hendraekky1946@gmail.com>
2023-10-29 15:39:10 +0700

    [Hendra] add Hero component into Home Page

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
 
 const Home = () => {
 
     return(
-        <Navbar />
+        <>
+            <Navbar />
+
+            <Hero />
+        </>
     );
     
 };
