1ec06d94ad8a9b3307ce4da81ef4e7e87371bbd7
Hendra1505 <hendraekky1946@gmail.com>
2025-09-20 13:39:54 +0700

    [Ekky] Add products Rest API to APP

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from .routers import product_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Rest API untuk E-commerce UMKM",
+    description="API untuk mengelola produk, pesanan, dan customer",
+    version="0.1.0"
+)
+
+# Daftarkan router produk ke aplikasi utama
+app.include_router(product_router.router)
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Welcome to API for UMKM E-Commerce"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
