3f5da72f09d471d99538773f5c47d9d3f1b66f77
Hendra1505 <hendraekky1946@gmail.com>
2025-09-21 22:25:50 +0700

    [Ekky] Create endpoint customer for api

--- backend/app/routers/customer_router.py
+++ backend/app/routers/customer_router.py
@@ -1,2 +1,18 @@
 from fastapi import APIRouter, HTTPException, status
 from typing import List
+from .. import schemas 
+from ..database import get_db_connection, release_db_connection
+
+router = APIRouter(
+    prefix='/customers',
+    tags=["Customers"]
+)
+
+@router.get("/", response_model=List[schemas.Customer])
+def get_all_customer():
+    conn = get_db_connection()
+    try:
+        cursor = conn.cursor()
+        query = "SELECT id, first_name, last_name, username, phone_number, email, religion, gender, profile_picture FROM customers ORDER BY id ASC;"
+        cursor.execute(query)
+        customer_tuples = cursor.fetchall()
