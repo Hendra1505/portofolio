0cb09a23e51d04c9c3630d9afa92ad78b144bc0b
Hendra1505 <hendraekky1946@gmail.com>
2025-11-03 13:19:10 +0700

    [Ekky] adding endpoint for brand api with method GET, POST, PUT and DELETE

--- backend/app/routers/brand_router.py
+++ backend/app/routers/brand_router.py
@@ -64,3 +64,77 @@ def create_new_brand(brand: schemas.BrandCreate):
         release_db_connection(conn)
         
 
+@router.put("/{id}", response_model=schemas.Brands, status_code=status.HTTP_200_OK, summary="Edit or Update brand value")
+def update_record_value_brands(id:int, brand: schemas.BrandCreate):
+    conn = get_db_connection()
+    try:
+        cursor = conn.cursor(cursor_factor=DictCursor)
+
+        if not brand:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Brand with id: {id} not found.")
+
+        query_update = """
+        UPDATE brands
+        SET name=%s
+        WHERE id = %s
+        RETURNING *;
+        """
+
+        cursor.execute(query_update, (
+            brand.name,
+            id
+        ))
+
+        updated_brand_row = cursor.fetchone()
+
+        if not updated_brand_row:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Brand with id: {id} not found.")
+        
+        conn.commit()
+
+        cursor.close()
+        return dict(updated_brand_row)
+    except Exception as e:
+        conn.rollback()
+        if isinstance(e, HTTPException):
+            raise e
+        if "violates foreign key constraint" in str(e):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Brand with id: {id} not found.")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="An unexpected error accured: {str(e)}")
+    finally:
+        release_db_connection(conn)
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, summary="Deleting a Brand")
+def delete_brand(id: int):
+    conn = get_db_connection()
+
+    try:
+        cursor = conn.cursor(cursory_factory=DictCursor)
+
+        query_delete = "DELETE FROM brands WHERE id = %s RETURNING id;"
+        cursor.execute(query_delete, (id,))
+
+        deleted_brand = cursof.fetchone()
+
+        if not deleted_brand:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Brand with id: {id} not found.")
+        
+        conn.commit()
+        cursor.close()
+        
+        return None
+    
+    except Exception as e:
+        conn.rollback()
+
+        if "foreign key constraint" in str(e).lower():
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Brand with id: {id} cannot deleted because is still in use by products.")
+        
+        if ininstance(e, HTTPException) and e.status_code == 404:
+            raise e
+        
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An unexpected error occured {str(e)}")
+    finally:
+        if conn:
+            release_db_connection(conn)
