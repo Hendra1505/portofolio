a3470725ec92aab4fbf6849f00037381a5d7c209
Hendra1505 <hendraekky1946@gmail.com>
2025-11-10 11:38:50 +0700

    [Ekky] Register cart_items

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import product_router, customer_router, brand_router, categories_router
+from .routers import product_router, customer_router, brand_router, categories_router, cart_items_router
 
 app = FastAPI(
     title="Rest API untuk E-commerce UMKM",
@@ -7,7 +7,7 @@ app = FastAPI(
     version="0.1.0"
 )
 
-# Register router produk
+# Register router product
 app.include_router(product_router.router)
 # Register router customer dan address buat customer ya guys ya
 app.include_router(customer_router.router)
@@ -15,6 +15,8 @@ app.include_router(customer_router.router)
 app.include_router(brand_router.router)
 # Register router categories
 app.include_router(categories_router.router)
+# Register router cart items
+app.include_router(cart_items_router.router)
 
 @app.get("/")
 def read_root():
